b3e2db58eb74dee086cb5ec1aa59a3d3327a1b88
puras <puras.he@gmail.com>
2016-10-06 01:19:52 +0800

    增加使用Template做为视图配置及演示

--- src/main/java/me/puras/se/controller/HomeController.java
+++ src/main/java/me/puras/se/controller/HomeController.java
@@ -1,17 +1,18 @@
 package me.puras.se.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by puras on 10/5/16.
  */
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("home")
     String home() {
         System.out.println("In Home");
-        return "Hello mo-se project!中文一下";
+        return "index";
     }
 }
